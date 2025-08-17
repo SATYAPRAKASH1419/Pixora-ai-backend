09aa675459c3d89cea14682b4f1f78269f93feff
SATYAPRAKASH1419 <satyaprakashswain37017@gmail.com>
2025-08-18 01:42:49 +0530

    changed server

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
-app.use(requireAuth());
 
-app.use('/api/ai',aiRouter)
-app.use('/api/user',userRouter)
+app.use('/api/ai',requireAuth(),aiRouter)
+app.use('/api/user',requireAuth(),userRouter)
 await connectCloudinary();
 
 
